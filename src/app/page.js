4a176448b01ec1f2ae729904b80e696e571858e5
gonzagomezp <1912988@ucc.edu.ar>
2024-03-11 19:21:46 -0300

    .

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+//import { Main } from "next/document";
+//import Image from "next/image";
+import Usuarios from "@/components/Usuarios";
+
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen">
+      <Usuarios />
+    </div>
+  );
+}
---
+++ src/components/Usuarios.jsx
@@ -0,0 +1,13 @@
+// components/Usuarios.js
+import React from "react";
+
+const Usuarios = () => {
+  return (
+    <div className="grid place-items-center grid-cols-1 h-screen w-full">
+      <h1>USUARIOS</h1>
+      <p>sofaoemo</p>
+    </div>
+  );
+};
+
+export default Usuarios;
